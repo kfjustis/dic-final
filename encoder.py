cc13c1730d34cd692d09b3f2e711a71d62ebd33d
kfjustis <kfjustis@gmail.com>
2017-05-07 13:34:38 -0500

    Removed excessive comments from encoder

--- encoder.py
+++ encoder.py
@@ -33,7 +33,6 @@ def main(argv):
 
     # apply DCT transform
     print("Applying DCT transform...")
-    #imgArrFloat = imgArr.astype(float)
     imgDCT = encoder.cv2.dct(imgArr.astype(float))
     print("\tTransform applied!")
 
@@ -50,36 +49,6 @@ def main(argv):
     enc = huffman.Encoder("raw.txt")
     enc.write("image.bit")
     print("\tEncoding complete!")
-
-    # testing zig-zag
-    '''
-    x = encoder.np.array([[1,2,3],[4,5,6],[7,8,9]], encoder.np.int8)
-    print(x)
-    newMat = ziggy.printZMatrix(x)
-    print(newMat)
-    '''
-
-    # test inverse transform
-    '''
-    imgIDCT = cv2.idct(imgDCT)
-    print(imgIDCT)
-    imgOrig = imgIDCT.astype("uint8")
-    print(imgOrig)
-    pass1 = encoder.Image.fromarray(imgOrig)
-    pass1.show()
-    '''
-
-    # test huffman coding
-    '''
-    testFile = "test1.txt"
-    compdFile = "compd.scw"
-    decompdFile = "test2.txt"
-    enc = huffman.Encoder(testFile)
-    enc.write(compdFile)
-    enc.write("test_read.txt")
-    dec = huffman.Decoder(compdFile)
-    dec.decode_as(decompdFile)
-    '''
 
 if __name__ == "__main__":
     main(sys.argv[1:])

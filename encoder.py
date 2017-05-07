29c05b7974111ca8d8d804b28c369250f4c55e04
kfjustis <kfjustis@gmail.com>
2017-05-07 12:06:29 -0500

    Renamed files to support encoder/decoder programs

--- encoder.py
+++ encoder.py
@@ -1,10 +1,62 @@
-import cv2
-from PIL import Image
-import numpy as np
-import pywt
+import encoder_lib as encoder
 
-def load_image_as_array(imgFile):
-    img = Image.open(imgFile)
-    imgArray = np.asarray(img)
+import huffman_lib as huffman
+import getopt
+import sys
 
-    return imgArray
+def main(argv):
+    inputFile = ""
+
+    # load file with command line args
+    try:
+        opts, args = getopt.getopt(argv,"i:")
+    except getopt.GetoptError:
+        print("USAGE: python3 main.py -i <file>")
+        sys.exit()
+
+    for opt, arg in opts:
+        if opt == "-i":
+            inputFile = arg
+        else:
+            print("USAGE: python3 main.py -i <file>")
+            sys.exit()
+
+    if (inputFile is ""):
+        print("USAGE: python3 main.py -i <file>")
+        sys.exit()
+
+    # load image as array
+    print("Loading image...")
+    imgArr = encoder.load_image_as_array(inputFile)
+    print("\tImage loaded!")
+
+    # apply DCT transform
+    print("Applying DCT transform...")
+    imgArrFloat = imgArr.astype(float)
+    imgDCT = encoder.cv2.dct(imgArrFloat)
+    print("\tTransform applied!")
+
+    # test inverse transform
+    '''
+    imgIDCT = cv2.idct(imgDCT)
+    print(imgIDCT)
+    imgOrig = imgIDCT.astype("uint8")
+    print(imgOrig)
+    pass1 = encoder.Image.fromarray(imgOrig)
+    pass1.show()
+    '''
+
+    # test huffman coding
+    '''
+    testFile = "test1.txt"
+    compdFile = "compd.scw"
+    decompdFile = "test2.txt"
+    enc = huffman.Encoder(testFile)
+    enc.write(compdFile)
+    enc.write("test_read.txt")
+    dec = huffman.Decoder(compdFile)
+    dec.decode_as(decompdFile)
+    '''
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
